6ce00484e90d457b13031c6c19400ac22756e6c0
Evan Emenegger <evan.emenegger@paramount.com>
2023-06-22 15:29:22 -0600

    add optional chaining in tile

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -6,14 +6,14 @@ import { setCurrentQuestion, showQuestion } from "../QuestionModal/questionSlice
 const Tile = ({ clueData, category }) => {
   // const {question, value, answer, category} = clueData; //* this doesn't destructure during some fetch calls, why?
   const dispatch = useDispatch();
-  const [answered, setAnswered] = useState(false)
+  const [answered, setAnswered] = useState(false);
 
   const handleClick = (e) => {
     e.preventDefault();
     // dispatch(setCurrentQuestion({question, value, answer, category}));
-    dispatch(setCurrentQuestion({ question: clueData.question, value: clueData.value, answer: clueData.answer, category }));
-    dispatch(showQuestion())
-    setAnswered(true)
+    dispatch(setCurrentQuestion({ question: clueData?.question, value: clueData?.value, answer: clueData?.answer, category }));
+    dispatch(showQuestion());
+    setAnswered(true);
   };
   if (answered) {
     return (
@@ -24,7 +24,7 @@ const Tile = ({ clueData, category }) => {
   } else {
     return (
       <div onClick={handleClick} className={styles.active}>
-        <h2>${clueData.value}</h2>
+        <h2>${clueData?.value}</h2>
       </div>
     );
   }
