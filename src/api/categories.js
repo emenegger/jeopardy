4c5772c542a7da37e9d35fb59799f08cf61bd70f
Evan Emenegger <evan.emenegger@paramount.com>
2023-07-06 14:33:10 -0600

    update round one values

--- src/api/categories.js
+++ src/api/categories.js
@@ -12,20 +12,28 @@ const CATEGORIES_QUERY = "https://jservice.io/api/category?id=";
 export const fetchCategoryDataById = async (categoryId, isDoubleJeopardy) => {
   const response = await fetch(`${CATEGORIES_QUERY}${categoryId}`);
   const data = await response.json();
-  console.log('data', data)
-  const firstRoundVals = [100, 200, 300, 400, 500];
-  const secondRoundVals = [600, 700, 800, 900, 1000];
+  console.log("data", data);
+  const initClues = data.clues; 
+  console.log("initClues", initClues);
+  const modifiedValsForFirstRound = initClues.map((clue) => ({
+    ...clue,
+    value: clue.value * 2,
+  }));
+  console.log("modifiedValsForFirstRound", modifiedValsForFirstRound);
+  const firstRoundVals = [200, 400, 600, 800, 1000];
+  const secondRoundVals = [400, 800, 1200, 1600, 2000];
   // for each of the round vals, find one and add
-  const getFilteredClues = (data, requiredVals) => {
+  const getFilteredClues = (clues, requiredVals) => {
     const returnArr = [];
     for (let val of requiredVals) {
-      const matchedVal = data.clues.find(clue => clue.value === val) || []
+      const matchedVal = clues.find((clue) => clue.value === val) || [];
       returnArr.push(matchedVal);
-    } 
+    }
     return returnArr;
   };
   const vals = isDoubleJeopardy ? secondRoundVals : firstRoundVals;
-  const filteredVals = getFilteredClues(data, vals);
+  const filteredVals = getFilteredClues(modifiedValsForFirstRound, vals);
   data.clues = filteredVals;
+  console.log(data.clues);
   return data;
 };
